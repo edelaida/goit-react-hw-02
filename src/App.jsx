1be980f1be73741d63c8f45e4a0b2ec3139edac1
Levina Tani <edelaida@gmail.com>
2024-03-16 21:58:40 +0200

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import Description from "./components/Description/Description";
+import Feedback from "./components/Feedback/Feedback";
+import Options from "./components/Options/Options";
+import { useState } from "react";
+
 function App() {
+  const [values, setValues] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
   return (
     <div>
       <h1>goit-react-hw-02</h1>
+      <Description />
+      <Options />
+      <Feedback />
     </div>
   );
 }
